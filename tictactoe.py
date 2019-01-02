6a6c3feea416bdbe2cbfacc017a8ac53952241c1
cara <cpanero@ucvts.org>
2019-01-02 14:58:39 +0000

    player 1

--- tictactoe.py
+++ tictactoe.py
@@ -11,5 +11,17 @@ class tic_tac_toe:
         print(self.board[6], self.board[7], self.board[8])
         print()
 
-game = tic_tac_toe(['1', '2', '3', '4', '5', '6', '7', '8', '9'],[(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)])
+    def player_one(self):
+        not_selected = True
+        while not_selected:
+            n = int(input("Player 1, chose where to put an X.   "))
+            if self.board[n] == "X" or self.board[n] == "O":
+                print("That spot is taken. Please pick another location.")
+            else:
+                self.board[n] = "X"
+                not_selected = False
+
+game = tic_tac_toe(['0', '1', '2', '3', '4', '5', '6', '7', '8'],[(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)])
+game.draw()
+game.player_one()
 game.draw()
